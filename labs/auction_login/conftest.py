295fb5f48cebc3b74f8f0ee303dc15e064accb2a
yelyzaveta.herasymenko <yelyzaveta.herasymenko@awwcor.com>
2025-02-10 11:41:48 +0200

    login_6_after_pr. Сommented out lines in conftest

--- labs/auction_login/conftest.py
+++ labs/auction_login/conftest.py
@@ -7,47 +7,47 @@ from labs.loging.creds import MAIN_URL, USER_AGENT
 
 
 # 1
-# @pytest.fixture(scope="class")
-# def driver(request):
-#     """Fixture for creating a driver without cookies"""
-#     driver = webdriver.Chrome()
-#     request.cls.driver = driver
-#     yield driver
-#     driver.quit()
-
-
-# 2
 @pytest.fixture(scope="class")
 def driver(request):
-    options = webdriver.ChromeOptions()
-    # options.add_argument('dom.webdriver.enabled', False)
-    options.add_argument(f"--user-agent={USER_AGENT}")
-    options.add_argument("--disable-blink-features=AutomationControlled")  # Remove Selenium detection
-    # options.add_argument("--incognito")  # Use incognito mode
-    # options.add_argument(
-    #    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Exclude automation switches
-    options.add_experimental_option("useAutomationExtension", False)
-
-    driver = webdriver.Chrome(options=options)
-    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-        "source": """
-            Object.defineProperty(navigator, 'webdriver', {
-                get: () => undefined
-            });
-        """
-    })
-    driver.execute_cdp_cmd(
-        'Network.setExtraHTTPHeaders',
-        {"headers": {
-            "Accept-Language": "en-US,en;q=0.9",
-            # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-            "User-Agent": USER_AGENT,
-        }}
-    )
+    """Fixture for creating a driver without cookies"""
+    driver = webdriver.Chrome()
     request.cls.driver = driver
     yield driver
     driver.quit()
+
+
+# 2
+# @pytest.fixture(scope="class")
+# def driver(request):
+#     options = webdriver.ChromeOptions()
+#     # options.add_argument('dom.webdriver.enabled', False)
+#     options.add_argument(f"--user-agent={USER_AGENT}")
+#     options.add_argument("--disable-blink-features=AutomationControlled")  # Remove Selenium detection
+#     # options.add_argument("--incognito")  # Use incognito mode
+#     # options.add_argument(
+#     #    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+#     options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Exclude automation switches
+#     options.add_experimental_option("useAutomationExtension", False)
+#
+#     driver = webdriver.Chrome(options=options)
+#     driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+#         "source": """
+#             Object.defineProperty(navigator, 'webdriver', {
+#                 get: () => undefined
+#             });
+#         """
+#     })
+#     driver.execute_cdp_cmd(
+#         'Network.setExtraHTTPHeaders',
+#         {"headers": {
+#             "Accept-Language": "en-US,en;q=0.9",
+#             # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+#             "User-Agent": USER_AGENT,
+#         }}
+#     )
+#     request.cls.driver = driver
+#     yield driver
+#     driver.quit()
 
 
 def pytest_runtest_makereport(item, call):
@@ -95,6 +95,6 @@ def pytest_runtest_teardown(item):
     Вызывается после выполнения теста.
     Применение: Очистка после теста, например, удаление временных данных или файлов."""
     print(f"Завершение теста {item.name}")
-    driver = item.funcargs.get("driver")
-    if driver:
-        driver.delete_all_cookies()
+    # driver = item.funcargs.get("driver")
+    # if driver:
+    #     driver.delete_all_cookies()
--- labs/auction_login/tests/test_auction_login_page.py
+++ labs/auction_login/tests/test_auction_login_page.py
@@ -42,11 +42,12 @@ class TestLoginPage:
         self.login_page.open_page(BASE_URL)
         self.login_page.maximize_window_position()
         time.sleep(3)
-        home_login_button = self.login_page.wait_element(self.login_page.HOME_LOGIN_BUTTON)
-        self.login_page.click_element(home_login_button)
+        self.login_page.find_click_on_signin_button()
+        # home_login_button = self.login_page.wait_element(self.login_page.HOME_LOGIN_BUTTON)
+        # self.login_page.click_element(home_login_button)
 
         # self.login_page.find_click_on_signin_button()
-        # self.login_page.maximize_window_position()
+
         email_input = self.login_page.search_element(EMAIL_FIELD)
         self.login_page.enter_text(email_input, USERNAME_EMAIL)
 
