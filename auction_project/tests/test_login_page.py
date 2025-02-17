98b22db39c3960cada8ebbb6750c788e49ec0f7a
yelyzaveta.herasymenko <yelyzaveta.herasymenko@awwcor.com>
2025-02-17 08:54:13 +0200

    auction_project_7. Modified test_login_page

--- auction_project/tests/test_login_page.py
+++ auction_project/tests/test_login_page.py
@@ -6,16 +6,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from auction_project.pages.login_page import LoginPage
 from auction_project.creds import BASE_URL, LOGIN_URL, USERNAME_EMAIL, PASSWORD, FORGOT_PASSWORD_URL
-from auction_project.locators import LocatorHomePage, LocatorLoginPage, LocatorAccountSettings
+from auction_project.locators.locators import LocatorHomePage, LocatorLoginPage, LocatorAccountSettings
 
 
 @pytest.mark.usefixtures("driver")
 class TestLoginPage:
-
-    @pytest.fixture(scope='function', autouse=True)
-    def cleaning_driver_cache(self):
-        yield
-        self.login_page.delete_all_cookies()
 
     def setup_method(self):
         self.login_page = LoginPage(self.driver)
@@ -135,7 +130,7 @@ class TestLoginPage:
         # 'Sign In' button is changed to 'Signing In…' button
         login_signin_button = self.login_page.search_element(LocatorLoginPage.LOGIN_SIGNIN_BUTTON)
         self.login_page.click_element(login_signin_button)
-
+# 1 ----------after click Signing In...-
         # self.login_page.wait_text_to_be_present_value(LocatorLoginPage.LOGIN_SIGNIN_BUTTON,
         #                                               LocatorLoginPage.LG_SIGNIN_BUTTON_TEXT_AFTER_CLICK)
         # login_signin_button = self.login_page.search_element(LocatorLoginPage.LOGIN_SIGNIN_BUTTON)
@@ -154,7 +149,8 @@ class TestLoginPage:
         self.login_page.maximize_window_position()
 
         # 2 Check 'Sign In' button is present and clickable
-        home_signin_button = self.login_page.wait_element_to_be_clickable(LocatorHomePage.HOME_SIGNIN_BUTTON)
+        home_signin_button = self.login_page.wait_element(LocatorHomePage.HOME_SIGNIN_BUTTON)
+        self.login_page.wait_element_to_be_clickable(home_signin_button)
 
         # 3 Click on 'Sign In' button
         self.login_page.click_element(home_signin_button)
@@ -176,10 +172,10 @@ class TestLoginPage:
             f"The title should be shown as {LocatorLoginPage.MOD_WIN_TITLE_TEXT}"
 
         # 6 Check error window description text
-        # Error window description text is shown
+        # -- Error window description text is shown
         window_description = self.login_page.search_element(LocatorLoginPage.WINDOW_DESCRIPTION)
         assert window_description.is_displayed(), "The Description block is not shown on the Modal Window"
-        # Error window description text is:
+        # -- Error window description text is:
         # "That username and password does not appear to match any accounts on record.
         # Check to make sure you've entered your case-sensitive password correctly.
         # If the problem persists, you may also make a request for your password to be reset."
@@ -190,7 +186,7 @@ class TestLoginPage:
         assert actual_text == expected_text, f"The Description text is not matched to the expected text {expected_text}"
 
         # 7 Check description text has link to forgotten_password page
-        # in text 'make a request for your password to be reset'
+        # -- in text 'make a request for your password to be reset'
         window_forgot_password_link = self.login_page.search_element(
             LocatorLoginPage.WINDOW_FORGOT_PASSWORD_LINK_TEXT).get_attribute("href")
         assert window_forgot_password_link == FORGOT_PASSWORD_URL, \
@@ -207,6 +203,12 @@ class TestLoginPage:
         red_x_icon_color = self.login_page.get_property_value_execute_script(
             red_x_icon_block, "::before", "color")
         assert red_x_icon_color == 'rgb(229, 77, 66)', "Incorrect red color for 'X' icon!"
+
+# 2 -----------red x button img---------- assert 'none'--
+#         red_x_icon_img = self.login_page.get_property_value_execute_script(
+#             red_x_icon_block, "", "background-image")
+#         assert red_x_icon_img == 'https://www.edgepipeline.com/images/session_messenger/stop-red.png', \
+#             "Image is not found for red 'X' icon!"
 
         # 9 Check Error window has close ('x') button at top right corner
         close_button = self.login_page.search_element(LocatorLoginPage.CLOSE_BUTTON_MODAL_WINDOW)
