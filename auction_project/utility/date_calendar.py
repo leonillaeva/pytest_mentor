f05c1d623ee5c431aa5a36c7c26ae4cc4041107f
yelyzaveta.herasymenko <yelyzaveta.herasymenko@awwcor.com>
2025-02-24 11:56:40 +0200

    auction_8_0006. Docstrings with descriptions were added to the functions in date_calendare

--- auction_project/utility/date_calendar.py
+++ auction_project/utility/date_calendar.py
@@ -52,69 +52,61 @@ class Calendar:
         date_items_list = [day, month, year]
         return date_items_list
 
+    def get_weekday(self, date_without_format):
+        """Get a weekday, short version
+        :return: <class 'str'>, -> Wed"""
+        return date_without_format.strftime("%a")
+
+    def get_short_weekday_date_in_events_block(self, date_without_format):
+        weekday_short = self.get_weekday(date_without_format)
+        month = date_without_format.month
+        day = date_without_format.day
+        date = f"{month}/{day}"
+        return f"{weekday_short} {date}"
+
 
 if __name__ == "__main__":
     # --------0006 select date
     calendar = Calendar()
     today_without_format = calendar.get_today_date_without_format()
-    today_in_format = calendar.get_date_in_format(today_without_format)
-    today_day = calendar.get_day_number(today_without_format)
+    # print(f"Today without format: {today_without_format}")
+
+
+    # print(today_without_format.strftime("%A"))
+    # print(today_without_format.strftime("%a"))
+    # today_in_format = calendar.get_date_in_format(today_without_format)
+    # today_day = calendar.get_day_number(today_without_format)
     # print(f"Today day: {today_day}")
+    #
+    # # print(f"Today: {today_without_format}, in format: {today_in_format}")
+    # # print(type(today_without_format))
+    # # print(type(today_in_format))
+    # # print("Today's list", calendar.get_date_items_list(today_without_format))
+    # #
+    # tomorrow_date = calendar.get_next_date(today_without_format, 1)
+    # # print(f"Tomorrow: {tomorrow}")
+    # tomorrow_day_number = calendar.get_day_number(tomorrow_date)
+    # # print("Day tomorrow: ", tomorrow_day_number)
+    # # print(type(tomorrow_day_number))
+    #
+    # # print("Tomorrow`s list", calendar.get_date_items_list(tomorrow_date))
+    # tomorrow_month = calendar.get_name_month(tomorrow_date)
+    # # print(tomorrow_month)
+    # # print(type(tomorrow_month))
+    # tomorrow_year = calendar.get_year(tomorrow_date)
+    # print(tomorrow_year)
+    # print(type(tomorrow_year))
+# -------------0007
+    # today_weekday = calendar.get_weekday(today_without_format)
+    # print(today_weekday)
+    # print(type(today_weekday))
+    today_short_date = calendar.get_short_weekday_date_in_events_block(today_without_format)
+    print(today_short_date)
 
-    # print(f"Today: {today_without_format}, in format: {today_in_format}")
-    # print(type(today_without_format))
-    # print(type(today_in_format))
-    # print("Today's list", calendar.get_date_items_list(today_without_format))
-    #
-    tomorrow_date = calendar.get_next_date(today_without_format, 1)
-    # print(f"Tomorrow: {tomorrow}")
-    tomorrow_day_number = calendar.get_day_number(tomorrow_date)
-    # print("Day tomorrow: ", tomorrow_day_number)
-    # print(type(tomorrow_day_number))
+    yesterday = calendar.get_next_date(today_without_format, -1)
+    print("Yesterday", yesterday)
 
-    # print("Tomorrow`s list", calendar.get_date_items_list(tomorrow_date))
-    tomorrow_month = calendar.get_name_month(tomorrow_date)
-    # print(tomorrow_month)
-    # print(type(tomorrow_month))
-    tomorrow_year = calendar.get_year(tomorrow_date)
-    print(tomorrow_year)
-    print(type(tomorrow_year))
-
-    # -----------------след день 0006
-    # 1. Кликнуть на календарь
-    # 2. Проверить что появился пикер.
-    #
-    # 3 выбрать следующий день
-    # 3.получить сегодняшний день
-    # - дату, месяц, год
-    #
-    # 4. Вычислить дату следующего дня
-    #
-    # 5. получить данные след дня
-    # - дату, месяц, год
-    #
-    # 6. Проверить месяц, год в пикере - сложить как в пикере
-    # - проверить, если соответствует -> выбирать число даты
-    # если не соответствует, найти правую стрелку, кликнуть по ней
-    # Проверить месяц, год в пикере
-    # - проверить, если соответствует -> выбирать число даты
-    #  7. Проверить что дата отображается в поле календаря.
-
-    # ----------------------------
-    # from datetime import datetime, timedelta
-    #
-    # # Using current time
-    # ini_time_for_now = datetime.now()
-    #
-    # # printing initial_date
-    # print(str(ini_time_for_now))
-    #
-    # # Calculating future dates
-    # # for two years
-    # future_date_after_2yrs = ini_time_for_now + timedelta(days=730)
-    #
-    # future_date_after_2days = ini_time_for_now + timedelta(days=2)
-    #
-    # # printing calculated future_dates
-    # print('future_date_after_2yrs:', str(future_date_after_2yrs))
-    # print('future_date_after_2days:', str(future_date_after_2days))
+    next_date_min = calendar.get_next_date(yesterday, -6)
+    # print(next_date_min) # 2025-02-15 16:28:47.962448
+    next_short_min = calendar.get_short_weekday_date_in_events_block(next_date_min)
+    print("Expected", next_short_min) # Sat 02/15
